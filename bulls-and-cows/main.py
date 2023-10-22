8e8d7858e3703eda8d81f63742977a0492738621
SanGyuk-Raccoon <56672129+SanGyuk-Raccoon@users.noreply.github.com>
2023-10-22 15:56:56 +0900

    Init bulls and cows (#5)
    * create bulls-and-cows directory
    
    * delete useless file
    
    * init bulls-and-cows
    
    ---------
    
    Co-authored-by: ebs.kim <ebs.kim@samsung.com>

---
+++ bulls-and-cows/main.py
@@ -0,0 +1,26 @@
+def generateNumber():
+    return
+def getUserInput():
+    return
+def validateInput():
+    return
+def compareAnswer():
+    # n(strike)
+    # n(Ball)
+    # print(n(strike, n(Ball)))
+    return
+def printResult(result):
+    return
+###### 실제 게임 동작
+
+answer = generateNumber()
+
+while True:
+    user_input = getUserInput()
+    validateInput(user_input)
+
+    compareAnswer(user_input)
+
+
+
+
