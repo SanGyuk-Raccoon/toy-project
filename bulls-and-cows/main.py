ce7df512cb9acc92e5e340b13445ecb17df93c5d
uoohoon <hoonixox@naver.com>
2023-10-24 14:32:43 +0900

    for 함수를 통한 validateInput 함수 구현

--- bulls-and-cows/main.py
+++ bulls-and-cows/main.py
@@ -5,8 +5,15 @@ def generateNumber():
 def getUserInput():
     user_answer = input("please enter the number: ")
     return user_answer
-def validateInput():
-    return
+def validateInput(answer, user_answer):
+    strike = 0
+    ball = 0
+    for i in range(len(answer)):
+        if answer[i] == user_answer[i]:
+            strike += 1
+        elif user_answer in answer:
+            ball += 1
+    return strike, ball
 def compareAnswer():
     # n(strike)
     # n(Ball)
