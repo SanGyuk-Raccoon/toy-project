7b6d4ae4b1696540c16e69ed02944d77bc8359eb
uoohoon <hoonixox@naver.com>
2023-10-24 15:53:56 +0900

    줄 맞춤 및 띄어쓰기  정리

--- bulls-and-cows/main.py
+++ bulls-and-cows/main.py
@@ -1,7 +1,8 @@
 def generateNumber():
     import random
-    answer = random.sample(range(0,10),3)
+    answer = random.sample(range(0, 10),3)
     return answer
+
 def getUserInput():
     user_answer = input("please enter the number: ")
     return user_answer
@@ -10,9 +11,11 @@ def validateInput(user_answer):
     if user_answer.isdigit() == False:
         print("error : please enter the NUMBER")
         return False
+
     if len(user_answer) != 3:
         print("error : please enter the 3-digits number")
         return False
+
     if len(set(user_answer)) != 3:
         print("error : please enter the UNIQUE number")
         return False
@@ -23,12 +26,14 @@ def compareAnswer(answer, user_answer):
     user_answer_list = list(map(int, str(user_answer)))
     strike = 0
     ball = 0
+
     for i in range(len(answer)):
         for j in range(len(user_answer_list)):
             if answer[i] == user_answer_list[j] and i == j:
                 strike += 1
             elif answer[i] == user_answer_list[j] and i != j:
                 ball += 1
+
     return strike, ball
 
 def printResult(strike, ball):
@@ -38,9 +43,9 @@ def printResult(strike, ball):
         print("you win!")
         return True
     else:
-        print("strike :", strike, "ball", ball)
-###### 실제 게임 동작
+        print(strike,"S ",ball,"B")
 
+###### 실제 게임 동작
 answer = generateNumber()
 print(answer)
 
