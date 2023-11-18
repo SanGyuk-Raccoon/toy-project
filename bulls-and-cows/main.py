dff547da3e2887e00dcfbbf97c9e2808922c0bb1
uoohoon <hoonixox@naver.com>
2023-11-18 10:39:40 +0900

    curses를 이용하여 user input받기

--- bulls-and-cows/main.py
+++ bulls-and-cows/main.py
@@ -1,14 +1,24 @@
 import logic
+import curses
 
-max_game_count = 10
-answer = logic.generateNumber()
-print(answer)
+MAX_GAME_COUNT = 10
 
-while max_game_count > 0:
-    user_answer = logic.getUserInput()
+stdscr = curses.initscr()
+curses.resize_term(100, 200)
+
+
+
+
+
+true_number = logic.generateNumber()
+
+while MAX_GAME_COUNT > 0:
+    stdscr.addstr(0, 0, "please enter the number:")
+    user_answer = stdscr.getstr(1, 0, 3).decode('utf-8')
+
     if logic.validateInput(user_answer):
-        max_game_count -= 1
-        strike_count, ball_count, out_count = logic.compareAnswer(answer, user_answer)
+        MAX_GAME_COUNT -= 1
+        strike_count, ball_count, out_count = logic.compareAnswer(true_number, user_answer)
         result = logic.printResult(strike_count, ball_count, out_count)
         if strike_count == 3:
             break
