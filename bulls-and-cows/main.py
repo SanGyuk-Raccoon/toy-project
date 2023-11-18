b0523c61d5afa9a90d5f19556f3b6d783ee8a93a
uoohoon <hoonixox@naver.com>
2023-11-18 10:56:13 +0900

    curses를 이용하여 게임 동작 확인

--- bulls-and-cows/main.py
+++ bulls-and-cows/main.py
@@ -7,18 +7,21 @@ stdscr = curses.initscr()
 curses.resize_term(100, 200)
 
 
-
-
-
 true_number = logic.generateNumber()
 
-while MAX_GAME_COUNT > 0:
+game_count = 0
+while game_count < MAX_GAME_COUNT:
     stdscr.addstr(0, 0, "please enter the number:")
     user_answer = stdscr.getstr(1, 0, 3).decode('utf-8')
-
     if logic.validateInput(user_answer):
-        MAX_GAME_COUNT -= 1
+        game_count += 1
         strike_count, ball_count, out_count = logic.compareAnswer(true_number, user_answer)
-        result = logic.printResult(strike_count, ball_count, out_count)
+
+
+        result = logic.getResult(strike_count, ball_count, out_count, game_count)
+        stdscr.addstr(2, 0, result)
+        stdscr.getkey()
         if strike_count == 3:
             break
+
+    stdscr.clear()
